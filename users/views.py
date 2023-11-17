65412adabc1439d58454af96e4ad65540d32faeb
Tatyana Kazakova <119956548+kavakoza@users.noreply.github.com>
2023-11-17 23:24:00 +0400

    initial commit

--- users/views.py
+++ users/views.py
@@ -9,32 +9,3 @@ class UserViewSet(viewsets.ModelViewSet):
 
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
-
-# from rest_framework import generics
-# from users.models import User
-# from users.serializers import UserSerializer
-# from rest_framework.permissions import AllowAny
-#
-#
-# class UserCreateAPIView(generics.CreateAPIView):
-#     serializer_class = UserSerializer
-#     permission_classes = [AllowAny]
-#
-#
-# class UserListAPIView(generics.ListAPIView):
-#     serializer_class = UserSerializer
-#     queryset = User.objects.all()
-#
-#
-# class UserRetrieveAPIView(generics.RetrieveAPIView):
-#     serializer_class = UserSerializer
-#     queryset = User.objects.all()
-#
-#
-# class UserUpdateAPIView(generics.UpdateAPIView):
-#     serializer_class = UserSerializer
-#     queryset = User.objects.all()
-#
-#
-# class UserDestroyAPIView(generics.DestroyAPIView):
-#     queryset = User.objects.all()
